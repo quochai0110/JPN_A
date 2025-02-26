45a5f6393cfa1dfa9ef451db2e11cafb103d0c3b
C4EJS100-TrinhQuocHai <trinhquochai12@gmail.com>
2025-02-26 15:12:00 +0700

    add session16

---
+++ session16/baitap/baitap1.js
@@ -0,0 +1,20 @@
+// khai biến fullName
+
+let fullName="nguyễn thị kim lệ";// kiểu dữ liệu string
+let _numberA=5; // khai báo kiểu dữ liệu number
+let checkEmail= true;// khai báo kiểu dữ liệu boolean
+// để kiểm tra kiểu dữ liệu dùng typeof
+
+// console.log("kiểu dữ liệu là:", typeof(checkEmail));
+console.log("1");
+console.log(a);
+
+console.log("2");
+
+
+
+
+
+
+
+
---
+++ session16/lythuyet/index.js
@@ -0,0 +1,12 @@
+document.write("cố gắng học js!");
+// prompt: hiển thị 1 ô cho người dùng nhập dữ liệu
+// viết chương trình yêu cầu người dùng nhập tuổi của mình
+
+let age= prompt("moi ban nhap tuoi");
+// console.log("giá trị bạn vừa nhập là",age);// hiển thị string bình thường
+// hiển thị theo kiểu template string
+
+// console.log(`gia trị bạn vừa nhập: ${age}`);
+alert("chào mừng bạn đến trang web!");
+
+
